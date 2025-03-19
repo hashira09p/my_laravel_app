b52dafb6375a3793c78cb5299aa3d144e61aecdd
jerome <jeromeabelida19@gmail.com>
2025-03-19 23:33:36 +0800

    added: design for index books views

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,11 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>List of Books</h1>
-    <a href="{{ route('books.create') }}">Add New Books</a>
-    <ul>
-        @foreach($books as $book)
-            <li>{{ $book->name }} ({{ $book->birth_date }})</li>
-        @endforeach
-    </ul>
+    <div class="container mt-4">
+        <h1 class="mb-4">Books</h1>
+
+        <a href="{{ route('books.create') }}" class="btn btn-primary mb-3">Add New Book</a>
+
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Published Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($books as $index => $book)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->author->name }}</td>
+                            <td>{{ $book->published_date }}</td>
+                            <td>
+                                <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('books.destroy', $book->id) }}" method="POST" class="d-inline">
+                                    @csrf @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Are you sure you want to delete this book?')">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
