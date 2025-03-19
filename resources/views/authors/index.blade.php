037b7490be589e2a1c1025984399b6283eb4bb73
jerome <jeromeabelida19@gmail.com>
2025-03-19 23:43:05 +0800

    modified: design in index views

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,41 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
-    <h1 class="mb-4">Authors</h1>
+    <div class="container mt-4">
+        <h1 class="mb-4">Authors</h1>
 
-    <a href="{{ route('authors.create') }}" class="btn btn-primary mb-3">Add Author</a>
+        <a href="{{ route('authors.create') }}" class="btn btn-primary mb-3">Add Author</a>
 
-    <div class="table-responsive">
-        <table class="table table-bordered table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($authors as $index => $author)
-                <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $author->name }}</td>
-                    <td>{{ $author->birth_date }}</td>
-                    <td>
-                        <a href="{{ route('authors.edit', $author->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                        <form action="{{ route('authors.destroy', $author->id) }}" method="POST" class="d-inline">
-                            @csrf @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm"
-                                onclick="return confirm('Are you sure you want to delete this author?')">
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Birth Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($authors as $index => $author)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $author->name }}</td>
+                            <td>{{ $author->birth_date }}</td>
+                            <td>
+                                <a href="{{ route('authors.edit', $author->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('authors.destroy', $author->id) }}" method="POST" class="d-inline">
+                                    @csrf @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Are you sure you want to delete this author?')">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
