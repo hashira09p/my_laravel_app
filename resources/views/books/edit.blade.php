b3e4c72f81ba9267a0fdc71e0b08d65feb6b8283
jerome <jeromeabelida19@gmail.com>
2025-03-19 23:42:26 +0800

    added: design for create and edit views for books

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -5,17 +5,18 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card shadow-lg">
-                    <div class="card-header bg-primary text-white">
-                        <h4 class="mb-0">Add Book</h4>
+                    <div class="card-header bg-warning text-white">
+                        <h4 class="mb-0">Edit Book</h4>
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('books.store') }}" method="POST">
+                        <form action="{{ route('books.update', $book->id) }}" method="POST">
                             @csrf
+                            @method('PUT')
 
                             <div class="mb-3">
                                 <label for="title" class="form-label">Book Title</label>
-                                <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}"
-                                    placeholder="Enter book title">
+                                <input type="text" name="title" id="title" class="form-control"
+                                    value="{{ old('title', $book->title) }}" placeholder="Enter book title">
                             </div>
 
                             <div class="mb-3">
@@ -23,18 +24,21 @@
                                 <select name="author_id" id="author_id" class="form-select">
                                     <option value="">-- Select an Author --</option>
                                     @foreach($authors as $author)
-                                        <option value="{{ $author->id }}">{{ $author->name }}</option>
+                                        <option value="{{ $author->id }}" {{ $author->id == $book->author_id ? 'selected' : '' }}>
+                                            {{ $author->name }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
 
                             <div class="mb-3">
                                 <label for="published_date" class="form-label">Published Date</label>
-                                <input type="date" name="published_date" id="published_date" class="form-control">
+                                <input type="date" name="published_date" id="published_date" class="form-control"
+                                    value="{{ old('published_date', $book->published_date) }}">
                             </div>
 
                             <div class="d-grid gap-2">
-                                <button type="submit" class="btn btn-success">Save Book</button>
+                                <button type="submit" class="btn btn-success">Update Book</button>
                                 <a href="{{ route('books.index') }}" class="btn btn-secondary">← Back to Books</a>
                             </div>
                         </form>
